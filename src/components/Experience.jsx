95e4d6445e2ce22ea140629afb7fd9720fc963d4
Rudra Agarwal <rudra.agarwal06@gmail.com>
2026-05-05 22:39:18 -0400

    Cut down

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-import { SiPython, SiNeo4J, SiReact, SiFastapi, SiAmazonwebservices, SiOpenai, SiLinux, SiGit, SiWolframmathematica, SiKhanacademy } from "react-icons/si";
+import { SiPython, SiNeo4J, SiReact, SiFastapi, SiAmazonwebservices, SiOpenai, SiLinux} from "react-icons/si";
 import { FaJava } from "react-icons/fa";
 import { Shield } from "lucide-react";
 
